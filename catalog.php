3d1bb9cd0b8a58c5d26bb7041c844fe6daa531c7
Имя <e-mail>
2018-01-18 12:01:27 +0300

    site

---
+++ catalog.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+    <body>
+        <div class="up">
+            <p>Декоративные решетки для камина</p>
+        </div>
+        <div class="logo">
+            <img src="img/logo3.jpg" alt="logo">
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="http://shop:8080/">Главная</a></li>
+                <li><a href="http://shop:8080/catalog.php">Каталог</a></li>
+                <li><a href="http://shop:8080/contacts.php">Контакты</a></li>
+            </ul>
+        </div>              
+                <!-- Разделы -->
+        <div class="flex_content">
+            <div class="row_container">               
+                <div class="flex1 sizer_item">
+                    <div class="flex_content_navi">
+                        <h4>Галерея</h4>
+                        <ul>
+                            <li><a href="">Классика</a></li>
+                            <li><a href="">Круглые</a></li>
+                            <li><a href="">Стандарт</a></li>
+                            <li><a href="">Дита</a></li>
+                            <li><a href="">Дэко</a></li>
+                            <li><a href="">Эксклюзив</a></li>
+                            <li><a href="">Тренд</a></li>
+                            <li><a href="">Стильные</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Колонки с товарами -->
+                <div class="flex2 row_container">
+                    <div class="flex1 direction_col">
+                        <div class="flex1">
+                            <div class="items sizer_item">
+                                <div>
+                                    <small>Классика</small>
+                                </div>
+                                <br>                                    
+                                <img src="img/foto1.jpg" height="70px" alt="нет изображения">                                 
+                                <p><strong>3 200 руб.</strong></p>
+                            </div>
+                        </div>
+                        <div class="flex1">
+                            <div class="items sizer_item">
+                                <div>
+                                    <small>Круглые</small>
+                                </div>
+                                <br>                                   
+                                <img src="img/foto2.jpg" height="70px" alt="нет изображения">
+                                <p><strong>4 500 руб.</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 2-я колонка -->
+                    <div class="flex1 direction_col">
+                        <div class="flex1">
+                            <div class="items sizer_item">
+                                <div>
+                                    <small>Стандарт</small>
+                                </div>
+                                <br>                                   
+                                <img src="img/foto3.jpg" height="70px" alt="нет изображения">                                   
+                                <p><strong>2 200 руб.</strong></p>
+                            </div>
+                        </div>
+                        <div class="flex1">
+                            <div class="items sizer_item">
+                                <div>
+                                    <small>Стильные</small>
+                                </div>
+                                <br>                                    
+                                <img src="img/foto4.jpg" height="70px" alt="нет изображения">                                
+                                <p><strong>8 400 руб.</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <br><br><br><br><br>      
+                    <!-- Footer -->
+                <div class="footer">
+                    <div class="col-12">
+                        <div class="footer-in">
+                            <p>Декоративные решетки для камина © 2017</p>
+                        </div>
+                    </div>                     
+                </div>
+</body>
+</html>
